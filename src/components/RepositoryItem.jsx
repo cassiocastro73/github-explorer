a2fa780a2cea1fb5d9686645e10c07a45a8b7365
Cassio <cassiocastro222@gmail.com>
2022-02-18 12:57:53 -0300

    First commit

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,11 @@
+export function RepositoryItem(){
+    return(
+        <li>
+            <strong>Unform</strong>
+            <p>Forms in ReactJS</p>
+            <a href="">
+                Acessar Repositório
+            </a>
+        </li>
+    );
+}
